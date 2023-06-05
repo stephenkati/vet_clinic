7a5d57e51a37ff6071e30d822fdb23562bcb27f3
stephenkati <stephenkatuli88@gmail.com>
2023-06-05 09:39:04 +0300

    insert more animals

--- data.sql
+++ data.sql
@@ -4,3 +4,12 @@ VALUES('Agumon', 'Feb 3, 2020', 0, TRUE, 10.23),
 ('Gabumon', 'Nov 15, 2018', 2, TRUE, 8),
 ('Pikachu', 'Jan 7, 2021', 1, FALSE, 15.04),
 ('Devimon', 'May 12, 2017', 5, TRUE, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Charmander', 'Feb 8, 2020', 0, FALSE, -11),
+('Plantmon', 'Nov 15, 2021', 2,TRUE,-5.7),
+('Squirtle', 'Apr 2, 1993', 3, FALSE, -12.13),
+('Angemon', 'Jun 12, 2005', 1, TRUE, -45),
+('Boarmon', 'Jun 7, 2005', 7, TRUE, 20.4),
+('Blossom', 'Oct 13, 1998', 3, TRUE, 17),
+('Ditto', 'May 14, 2022', 4, TRUE, 22);
